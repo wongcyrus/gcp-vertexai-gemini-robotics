07369125995f652f6d0f3dd7ee5b702dfc6366c5
Cyrus Wong <cywong@vtc.edu.hk>
2025-06-21 10:43:14 +0000

    Update ROBOT_API_URL in config.py to use localhost for local testing

--- robot-adk-agent/app/config.py
+++ robot-adk-agent/app/config.py
@@ -14,10 +14,10 @@ config = {
                 "server.py",
             ],
             "env": {
-                "ROBOT_API_URL": "https://6mz6soy3j3.execute-api.us-east-1.amazonaws.com/prod/run_action/",
                 # "ROBOT_API_URL": "https://humanoid-robot-simulator-74gfpibg5q-uc.a.run.app/run_action/",
+                "ROBOT_API_URL": "http://localhost:5000/run_action/",
+                "SESSION_KEY": "gCS8UbctmwkjjGfiHSdzyMrLMxIXt7AfrYN3XJXra6b%2FwYFjhY3hwqCayM2XL%2F3Azz79pvCXiSviu9FKFZ42QHZxiR6Wy%2FBemIaRzVdye8w%3D",
                 "ROBOT_IMAGE_API_URL": "http://127.0.0.1:5000:5000/run_action/",
-                "SESSION_KEY": "cywong@vtc.edu.hk",
             },
         },
     }
